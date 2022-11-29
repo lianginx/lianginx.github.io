c45717a739cbf1558a2b658943985b20bbc4e3d0
Liang <njl1@foxmail.com>
2022-11-29 09:56:32 +0800

    Fix

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   base: "/tech-note/",
   lang: "zh-CN",
   title: "Liang INX",
-  description: "描述",
+  // description: "描述",
   // titleTemplate: "标题模板",
   appearance: true, // true | false | "dark"
   lastUpdated: true, // 显示更新时间（Git 提交时间）

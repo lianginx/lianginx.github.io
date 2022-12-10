8f7ad9b6f4232d7777aac0348bd35c44aa4daadd
Liang <njl1@foxmail.com>
2022-12-10 11:13:18 +0800

    页面导航调整： 1. 暂时移除About入口，因为不知道写什么 2. 增加两个工具箱应用入口

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -6,5 +6,12 @@ export default [
   { text: "首页 HOME", link: "/" },
   { text: "技术 TECH", link: "/tech/" },
   { text: "日常 LIFE", link: "/life/" },
-  { text: "关于我 ME", link: "/readme/about.md" },
+  // { text: "关于我 ME", link: "/readme/about.md" },
+  {
+    text: "工具",
+    items: [
+      { text: "D-Utils", link: "http://server.in-x.cc/" },
+      { text: "Tools 工具箱", link: "https://tools.in-x.cc/" },
+    ],
+  },
 ] as DefaultTheme.NavItem[];

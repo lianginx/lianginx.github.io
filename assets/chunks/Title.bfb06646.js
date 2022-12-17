a68f434f0f299b94fedac4a3df2324dbdbbc7e2b
liang-n <liang-n@users.noreply.github.com>
2022-12-17 17:46:37 +0000

    deploy: 42db5b5b0d89d23d5d42d746aa723377bc006e33

---
+++ assets/chunks/Title.bfb06646.js
@@ -0,0 +1,1 @@
+import{d as l,c as _,a as e,t as s,o as n,_ as o}from"../app.cc322501.js";const a={class:"title"},i={class:"subtitle"},c=l({__name:"Title",props:{title:null,subtitle:null},setup(t){return(d,u)=>(n(),_("div",null,[e("div",a,[e("h1",null,s(t.title),1),e("div",i,s(t.subtitle),1)])]))}});const p=o(c,[["__scopeId","data-v-9f3b8eb2"]]);export{p as T};

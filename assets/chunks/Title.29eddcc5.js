9ce0b2123037fb66e57d167d567317a307e31314
lianginx <lianginx@users.noreply.github.com>
2023-02-25 01:43:40 +0000

    deploy: c3feae765aa6592465fdf21db568161ecbe01486

--- assets/chunks/Title.29eddcc5.js
+++
@@ -1,1 +0,0 @@
-import{d as l,c,a as t,t as s,o as _,_ as n}from"../app.87ca0ab1.js";const o={class:"title"},a={class:"subtitle"},i=l({__name:"Title",props:{title:null,subtitle:null},setup(e){return(d,u)=>(_(),c("div",null,[t("div",o,[t("h1",null,s(e.title),1),t("div",a,s(e.subtitle),1)])]))}});const p=n(i,[["__scopeId","data-v-3c3c8665"]]);export{p as T};

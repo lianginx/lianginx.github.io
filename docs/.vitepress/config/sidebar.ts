f20b80aa79948749bc9e0ccbf908163910c31a8e
Liang INX <liang@in-x.cc>
2023-03-05 22:47:41 +0800

    发布： - 本地存储：localStorage 和 sessionStorage 的差异 - 🍅番茄炖牛腩

--- docs/.vitepress/config/sidebar.ts
+++ docs/.vitepress/config/sidebar.ts
@@ -18,6 +18,10 @@ function tech(): DefaultTheme.SidebarGroup[] {
       collapsible: true,
       items: [
         {
+          text: "本地存储：localStorage 和 sessionStorage 的差异",
+          link: "/tech/frontend/js-local-storage.md",
+        },
+        {
           text: "JavaScript 遍历数组的方法",
           link: "/tech/frontend/js-traversal-array.md",
         },
@@ -129,6 +133,13 @@ function life(): DefaultTheme.SidebarGroup[] {
           text: "Keychron K2 键盘使用说明",
           link: "/life/keychron-k2.md",
         },
+      ],
+    },
+    {
+      text: "美食笔记",
+      collapsible: true,
+      items: [
+        { text: "🍅番茄炖牛腩", link: "/life/stewed-beef-brisket-with-tomato.md" },
       ],
     },
     {

ac59bf6e3682baef43611f3a69e5a42ec03c7797
LiangINX <liang@in-x.cc>
2023-04-01 23:30:51 +0800

    增加 ChatGPT 入口，移除失效的工具箱

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -8,12 +8,12 @@ export default [
   { text: "日常 LIFE", link: "/life/" },
   { text: "应用 APP", link: "/app/" },
   { text: "书籍 BOOK", link: "/book/" },
-  {
-    text: "工具 TOOL",
-    items: [
-      { text: "D-Utils", link: "http://server.in-x.cc/" },
-      { text: "Tools 工具箱", link: "https://tools.in-x.cc/" },
-    ],
-  },
+  // {
+  //   text: "工具 TOOL",
+  //   items: [
+  //     { text: "D-Utils", link: "http://server.in-x.cc/" },
+  //     { text: "Tools 工具箱", link: "https://tools.in-x.cc/" },
+  //   ],
+  // },
   // { text: "关于我 ME", link: "/readme/about.md" },
 ] as DefaultTheme.NavItem[];
